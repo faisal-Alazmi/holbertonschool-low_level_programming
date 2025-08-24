e2148e549108fbc1b3ac0e8c757062937bec31ff
root <root@ip-10-42-57-103.ec2.internal>
2025-08-24 17:48:44 +0000

    Add 6-size.c

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  * main - Prints the size of various types
  *
@@ -10,9 +9,9 @@ int main(void)
 	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(char));
 	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(int));
 	printf("Size of a long int: %lu byte(s)\n",
-                        (unsigned long)sizeof(long int));
+	       (unsigned long)sizeof(long int));
 	printf("Size of a long long int: %lu byte(s)\n",
-                        (unsigned long)sizeof(long long int));
+	       (unsigned long)sizeof(long long int));
 	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(float));
 	return (0);
 }
