22ac61d6e0b8d1ad81ecff73a8bfbc0b1f9036f3
root <root@ip-10-42-88-149.ec2.internal>
2025-08-26 14:51:38 +0000

    add 8

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: Prints all the numbers of base 16 in lowercase,
+ * followed by a new line
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char ch;
+for (ch = '0'; ch <= '9'; ch++)
+putchar(ch);
+for (ch = 'a'; ch <= 'z'; ch++)
+putchar(ch);
+putchar('\n');
+return (0);
+}
