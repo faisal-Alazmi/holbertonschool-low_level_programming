2427a1832056227a19b4790fdfa51cab0a3e2068
root <root@ip-10-42-88-149.ec2.internal>
2025-08-26 15:08:21 +0000

    add 9

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ *jbcjkdbcnkjnkjnkjzxn zxk
+ * lkcjxlkjclzjlkxjlk
+ * kjckxjklk
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i;
+for (i = 0; i <= 9; i++)
+{
+putchar(i + '0');
+if (i != 0)
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
