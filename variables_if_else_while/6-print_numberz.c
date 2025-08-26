0bfbbaaa453894a72c1982ce5ab5139d40495b1c
root <root@ip-10-42-88-149.ec2.internal>
2025-08-26 14:34:37 +0000

    6-print_numberz

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: Prints all single digit numbers of base 10 starting from 0,
+ * followed by a new line. Only putchar is used, no char variables allowed.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i;
+for (i = 0; i <= 9; i++)
+putchar(i + '0');
+putchar('\n');
+return (0);
+}
