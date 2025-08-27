b3f39fd7ac4532cb454f52508a0b4a05e31cfc68
faisal-Alazmi <fa41sa@hotmail.com>
2025-08-27 13:45:44 +0000

    update

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints 10 times the alphabet in lowercase
+ *
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+    int i;
+    char c;
+
+    for (i = 0; i < 10; i++) /* كرر 10 مرات */
+    {
+        for (c = 'a'; c <= 'z'; c++) /* من a إلى z */
+        {
+            _putchar(c);
+        }
+        _putchar('\n'); /* سطر جديد بعد كل أبجدية */
+    }
+}
