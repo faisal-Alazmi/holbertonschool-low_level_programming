bbb0fcd174538f9c180e197bd3477e7e86558513
root <root@ip-10-42-121-51.ec2.internal>
2025-09-01 15:25:43 +0000

    Add

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -4,15 +4,15 @@
  */
 void more_numbers(void)
 {
-	int r, n;
-	for (r = 0; r < 10; r++)
-	{
-		for (n = 0; n <= 14; n++)
-		{
-			if (n >= 10)
-				_putchar('1');
-			_putchar((n % 10) + '0');
-		}
-		_putchar('\n');
-	}
+int r, n;
+for (r = 0; r < 10; r++)
+{
+for (n = 0; n <= 14; n++)
+{
+if (n >= 10)
+_putchar('1');
+_putchar((n % 10) + '0');
+}
+_putchar('\n');
+}
 }
