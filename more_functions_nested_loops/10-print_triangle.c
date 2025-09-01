782c095819e663046caea1e4d7740414b7ac4a17
root <root@ip-10-42-99-48.ec2.internal>
2025-09-01 22:00:49 +0000

    Add 10

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_triangle - prints a triangle using '#' then a newline
+ * @size: size of the triangle
+ */
+void print_triangle(int size)
+{
+int row, sp, hash;
+if (size <= 0)
+{
+_putchar('\n');
+return;
+}
+for (row = 1; row <= size; row++)
+{
+for (sp = size - row; sp > 0; sp--)
+_putchar(' ');
+for (hash = 0; hash < row; hash++)
+_putchar('#');
+_putchar('\n');	
+}
+}
