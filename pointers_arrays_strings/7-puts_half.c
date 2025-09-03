f16a8229683207efa16bc02686309eee5fec1a53
root <root@ip-10-42-98-251.ec2.internal>
2025-09-03 15:15:31 +0000

    testing

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -10,16 +10,12 @@ void puts_half(char *str)
 int len = 0;
 int start, i;
 while (str[len] != '\0')
-{
 len++;
-}
 if (len % 2 == 0)
 start = len / 2;
 else 
 start = (len + 1) / 2;
 for (i = start; i < len; i++)
-{
 _putchar(str[i]);
-}
 _putchar('\n');
 }
