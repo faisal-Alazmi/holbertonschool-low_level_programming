2abeeb16468dfa905086431e5463bd433af2bc2b
root <root@ip-10-42-98-251.ec2.internal>
2025-09-03 16:35:28 +0000

    testing

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <limits.h>
 /**
  * _atoi - converts a string to an integer
  * @s: pointer to the string
@@ -11,9 +10,7 @@ int _atoi(char *s)
 int i = 0;
 int sign = 1;
 int result = 0;
-int digit;
-int found_digit = 0;
-while (s[i] == ' ' || s[i] == '\t' || s[i] == '\n' ||s[i] == '\r' || s[i] == '\v' || s[i] == '\f')
+while (s[i] == ' ' || s[i] == '\t' || s[i] == '\n' || s[i] == '\r' || s[i] == '\v' || s[i] == '\f')
 i++;
 while (s[i] == '+' || s[i] == '-')
 {
@@ -21,24 +18,12 @@ if (s[i] == '-')
 sign = -sign;
 i++;
 }
+if (s[i] < '0' || s[i] > '9')
+return (0);
 while (s[i] >= '0' && s[i] <= '9')
 {
-found_digit = 1;
-digit = s[i] - '0';
-if (sign == 1)
-{
-if (result > (INT_MAX - digit) / 10)
-return INT_MAX;
-}
-else
-{
-if (-result < (INT_MIN + digit) / 10)
-return INT_MIN; 
-}
-result = result * 10 + digit;
+result = result * 10 + (s[i] - '0');
 i++;
 }
-if (!found_digit)
-return 0;
-return result * sign;
+return (result * sign);
 }
