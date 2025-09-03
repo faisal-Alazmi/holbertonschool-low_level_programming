7b153fa24cf040ee5ed8f5c9f7256da5d4b95c2c
root <root@ip-10-42-98-251.ec2.internal>
2025-09-03 16:45:04 +0000

    testing

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,29 +1,57 @@
 #include "main.h"
+#include <limits.h>
 /**
  * _atoi - converts a string to an integer
  * @s: pointer to the string
  *
  * Return: the integer value, or 0 if no number found
+ *
+ * Behavior (Holberton-style):
+ * - Scan from start; before the first digit:
+ *   every '-' flips the sign, '+' is ignored, other chars are ignored.
+ * - Once the first digit appears, parse contiguous digits then stop.
+ * - If no digits at all, return 0.
+ * - Handle INT_MAX/INT_MIN without signed overflow.
  */
 int _atoi(char *s)
 {
 int i = 0;
 int sign = 1;
 int result = 0;
-while (s[i] == ' ' || s[i] == '\t' || s[i] == '\n' || s[i] == '\r' || s[i] == '\v' || s[i] == '\f')
-i++;
-while (s[i] == '+' || s[i] == '-')
+int started = 0;
+while (s[i] != '\0' && !started)
 {
 if (s[i] == '-')
 sign = -sign;
+else if (s[i] >= '0' && s[i] <= '9')
+started = 1;
 i++;
 }
-if (s[i] < '0' || s[i] > '9')
+if (!started)
 return (0);
+i--;
+if (sign == 1)
+{
 while (s[i] >= '0' && s[i] <= '9')
 {
-result = result * 10 + (s[i] - '0');
+int d = s[i] - '0';
+if (result > (INT_MAX - d) / 10)
+return (INT_MAX);
+result = result * 10 + d;
 i++;
 }
-return (result * sign);
+return (result);
+}
+else
+{
+while (s[i] >= '0' && s[i] <= '9')
+{
+int d = s[i] - '0';
+if (result < (INT_MIN + d) / 10)
+return (INT_MIN);
+result = result * 10 - d;
+i++;
+}
+return (result);
+}
 }
