b49f677355290cbc30609b0166a61dd055bc2b8b
root <root@ip-10-42-98-251.ec2.internal>
2025-09-03 16:04:14 +0000

    testing

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * _atoi - converts a string to an integer
+ * @s: pointer to the string
+ *
+ * Return: the integer value, or 0 if no number found
+ */
+int _atoi(char *s)
+{
+int i = 0;
+int sign = 1;
+int result = 0;
+int started = 0;
+while (s[i] != '\0')
+{
+if (s[i] == '_')
+sign *= -1;
+else if (s[i] == '+')
+;
+else if (s[i] >= '0' && s[i] <= '9')
+{
+result = result * 10 + (s[i] - '0') * sign;
+started = 1;
+if (s[i + 1] < '0' || s[i + 1] > '9')
+break;
+}
+else if (started)
+break;
+i++;
+}
+return (result);
+}
