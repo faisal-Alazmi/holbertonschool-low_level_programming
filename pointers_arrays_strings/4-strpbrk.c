de49724c24a157e96a69520846657a57eb6f876f
root <root@ip-10-42-82-19.ec2.internal>
2025-09-09 15:37:49 +0000

    testing

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _strpbrk - searches a string for any of a set of bytes
+ * @s: the string to search
+ * @accept: the string containing the bytes to match
+ *
+ * Return: pointer to the first matching byte in s,
+ *         or NULL if no match is found
+ */
+char *_strpbrk(char *s, char *accept)
+{
+char *a;
+while (*s)
+{
+for (a = accept; *a; a++)
+{
+if (*s == *a)
+return (*s);
+}
+s++;
+}
+return (0);
+}
