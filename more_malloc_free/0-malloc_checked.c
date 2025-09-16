eb31e4e1f8a8df40de3854df848ef5519a922d9d
root <root@ip-10-42-182-82.ec2.internal>
2025-09-16 20:27:10 +0000

    0-malloc_checked.c

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * malloc_checked - allocates memory using malloc
+ * @b: number of bytes to allocate
+ *
+ * Return: pointer to allocated memory
+ *         exits with status 98 if malloc fails
+ */
+void *malloc_checked(unsigned int b)
+{
+void *p;
+p = malloc(b);
+if (p == NULL)
+exit(98);
+return (p);
+}
---
+++ more_malloc_free/README.md
@@ -0,0 +1,1 @@
+Add more malloc free
