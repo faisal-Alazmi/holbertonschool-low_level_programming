b187e2b5311890c7909492e27ef30bb976888e63
root <root@ip-10-42-15-44.ec2.internal>
2025-09-17 10:37:05 +0000

    dog.h

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,15 +1,17 @@
 #ifndef DOG_H
 #define DOG_H
+
 /**
  * struct dog - معلومات عن كلب
  * @name: اسم الكلب (string)
  * @age: عمر الكلب (float)
  * @owner: صاحب الكلب (string)
  */
-struct dog 
+struct dog
 {
 char *name;
 float age;
 char *owner;
 };
+
 #endif
