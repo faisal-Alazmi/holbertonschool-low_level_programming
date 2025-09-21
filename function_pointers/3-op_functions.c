053f3dbc94fb7636558510a63c238e1241ea6978
root <root@ip-10-42-38-43.ec2.internal>
2025-09-21 09:13:41 +0000

    3-op_functions.c

---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,36 @@
+#include "3-calc.h"
+/**
+ * op_add - sum
+ */
+int op_add(int a, int b)
+{
+return (a + b);
+}
+/**
+ * op_sub - difference
+ */
+int op_sub(int a, int b)
+{
+return (a - b);
+}
+/**
+ * op_mul - product
+ */
+int op_mul(int a, int b)
+{
+return (a * b);
+}
+/**
+ * op_div - division
+ */
+int op_div(int a, int b)
+{
+return (a / b);
+}
+/**
+ * op_mod - modulo
+ */
+int op_mod(int a, int b)
+{
+return (a % b);
+}
