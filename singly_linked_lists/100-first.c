7b9c5be575ddd1e81460b222775139b03ff8614c
root <root@ip-10-42-253-222.ec2.internal>
2025-10-14 13:09:46 +0000

    100-first.c

---
+++ singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * first - Prints a message before the main function is executed.
+ *
+ * Return: void
+ */
+void __attribute__((constructor)) first(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
