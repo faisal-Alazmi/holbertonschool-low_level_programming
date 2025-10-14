bab806b2d3c598565c09a893344484b1a1bc9c6d
root <root@ip-10-42-183-149.ec2.internal>
2025-10-14 12:26:59 +0000

    2-add_node.c

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -1,6 +1,36 @@
 #include "lists.h"
 #include <stdlib.h>
-#include <string.h>
+
+/* Helpers (محلية داخل هذا الملف فقط) */
+static size_t _strlen_local(const char *s)
+{
+	size_t len = 0;
+
+	if (!s)
+		return (0);
+	while (s[len])
+		len++;
+	return (len);
+}
+
+static char *_strdup_local(const char *s)
+{
+	size_t i, len;
+	char *p;
+
+	if (!s)
+		return (NULL);
+
+	len = _strlen_local(s);
+	p = malloc(len + 1);
+	if (!p)
+		return (NULL);
+
+	for (i = 0; i < len; i++)
+		p[i] = s[i];
+	p[len] = '\0';
+	return (p);
+}
 
 /**
  * add_node - Adds a new node at the beginning of a list_t list.
@@ -13,13 +43,16 @@ list_t *add_node(list_t **head, const char *str)
 {
 	list_t *node;
 	char *dup;
+	unsigned int len;
 
 	if (head == NULL || str == NULL)
 		return (NULL);
 
-	dup = strdup(str);
+	dup = _strdup_local(str);
 	if (dup == NULL)
 		return (NULL);
+
+	len = (unsigned int)_strlen_local(dup);
 
 	node = malloc(sizeof(*node));
 	if (node == NULL)
@@ -29,7 +62,7 @@ list_t *add_node(list_t **head, const char *str)
 	}
 
 	node->str = dup;
-	node->len = (unsigned int)strlen(dup);
+	node->len = len;
 	node->next = *head;
 	*head = node;
 
