ec491fdb48c0b6037f7a216df8c64b1a5650cb51
root <root@ip-10-42-65-217.ec2.internal>
2025-10-15 15:15:37 +0000

    4-free_dlistint.c

---
+++ doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+/**
+ * free_dlistint - frees a dlistint_t list
+ * @head: head pointer
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *tmp;
+
+	while (head)
+	{
+		tmp = head->next;
+		free(head);
+		head = tmp;
+	}
+}
