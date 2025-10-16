f13731cd1493fd0e83663b631b8d5a86cfcc936b
root <root@ip-10-42-11-129.ec2.internal>
2025-10-16 12:47:50 +0000

    0-read_textfile.c

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,50 @@
+#include "main.h"
+
+/**
+ * read_textfile - reads a text file and prints it to POSIX stdout
+ * @filename: path to file
+ * @letters: max bytes to read
+ *
+ * Return: number of bytes actually printed, or 0 on error
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	ssize_t r, w, total = 0;
+	char *buf;
+
+	if (!filename || letters == 0)
+		return (0);
+
+	buf = malloc(letters);
+	if (!buf)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+	{
+		free(buf);
+		return (0);
+	}
+
+	r = read(fd, buf, letters);
+	if (r <= 0)
+	{
+		free(buf);
+		close(fd);
+		return (0);
+	}
+
+	w = write(STDOUT_FILENO, buf, r);
+	if (w != r)
+	{
+		free(buf);
+		close(fd);
+		return (0);
+	}
+
+	total = w;
+	free(buf);
+	close(fd);
+	return (total);
+}
