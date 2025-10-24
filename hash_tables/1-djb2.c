c78bebb447eef1cfb990028e407e63a5d24c5883
root <root@ip-10-42-192-227.ec2.internal>
2025-10-24 11:21:04 +0000

    Fix Betty style and comments in 1-djb2.c

--- hash_tables/1-djb2.c
+++ hash_tables/1-djb2.c
@@ -1,12 +1,21 @@
+#include "hash_tables.h"
+
+/**
+ * hash_djb2 - djb2 hash function
+ * @str: input string (as unsigned char *)
+ *
+ * Return: computed hash as unsigned long int
+ *
+ * Description:
+ * Implements the djb2 algorithm by Dan Bernstein.
+ */
 unsigned long int hash_djb2(const unsigned char *str)
 {
-    unsigned long int hash;
-    int c;
+	unsigned long int hash = 5381;
+	int c;
 
-    hash = 5381;
-    while ((c = *str++))
-    {
-        hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
-    }
-    return (hash);
+	while ((c = *str++))
+		hash = ((hash << 5) + hash) + c;
+
+	return (hash);
 }
